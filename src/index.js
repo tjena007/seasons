0c31b7a0c47db10deb965bdb069746cc52f17fa5
Tanmay Jena <tjena2@gmail.com>
2021-03-24 13:36:01 +0530

    Refactored to add class, location acquiring

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  render(){
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position.coords.latitude),
+      err => console.log(err)
+    );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    return <div>Latitude: </div>
+  }
+}
+ReactDOM.render(<App/>,document.querySelector('#root'));
