43ab1d2f439ac4ee0965fbaa311eb232b21189b8
Tanmay Jena <tjena2@gmail.com>
2021-03-26 16:49:51 +0530

    added content to display based on location and weather

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,38 @@
 import React from 'react'
 
-const SeasonDisplay = () => {
-  
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach!",
+        iconName: 'sun'
+    },
+    winter: {
+        text: "Brr, it's chilly!!",
+        iconName: 'snowflake'
+    }
+}
+
+const getSeason = (lat,month) => {
+    //console.log(lat,month)
+    if( month > 2 && month < 9){
+        return  lat> 0? 'summer' : 'winter';
+    } else {
+        return  lat> 0? 'winter' : 'summer';
+    }
+
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const {text, iconName} = seasonConfig[season];
+
+    //console.log(season);
+    return (
+        <div> 
+            <i className={`${iconName} icon`} />
+            <h1>{ text}</h1>
+            <i className={`${iconName} icon`} />
+        </div>
+    )
 }
 
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SeasonDisplay from './SeasonDisplay';
+
 class App extends React.Component {
-  constructor(props){
-    super(props);
+  state = { lat : null, errorMessage : ''}; 
 
-    this.state = { lat : null, errorMessage : ''};
-
+  componentDidMount(){
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat : position.coords.latitude})
-      },
+      position => this.setState({ lat : position.coords.latitude}),
       err => this.setState({errorMessage : err.message })
     );
   }
@@ -23,7 +21,11 @@ class App extends React.Component {
         return <div> ....Loading</div>
       }
       if(!this.state.errorMessage && this.state.lat){
-       return <div>Latitude: {this.state.lat}</div>
+       return (
+        <div>
+            <SeasonDisplay lat={this.state.lat} />
+        </div>
+       ) 
       }
 
   }
